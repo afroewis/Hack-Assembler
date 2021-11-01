a21acef41c2d013e27118067d913bfaf09daa015
Andreas Fröwis <andreas.froewis@gmail.com>
2021-11-01 10:49:00 +0100

    Implemented support for command-line arguments

--- assembler.go
+++ assembler.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var variables = make(map[string]int)
@@ -15,116 +17,133 @@ var output = make([]string, 0)
 var labels = make(map[string]int)
 var predefinedSymbols = make(map[string]int)
 var variablesMemoryStart = 16
+
 func main() {
+	if len(os.Args) == 1 {
+		log.Println("No Hack source file provided.")
+		return
+	}
+
 	predefinedSymbols = map[string]int{
-		"SO": 0,
-		"LCL": 1,
-		"ARG": 2,
-		"THIS": 3,
-		"THAT": 4,
-		"R0": 0x00,
-		"R1": 0x01,
-		"R2": 0x02,
-		"R3": 0x03,
-		"R4": 0x04,
-		"R5": 0x05,
-		"R6": 0x06,
-		"R7": 0x07,
-		"R8": 0x08,
-		"R9": 0x09,
-		"R10": 0x0a,
-		"R11": 0x0b,
-		"R12": 0x0c,
-		"R13": 0x0d,
-		"R14": 0x0e,
-		"R15": 0x0f,
+		"SO":     0,
+		"LCL":    1,
+		"ARG":    2,
+		"THIS":   3,
+		"THAT":   4,
+		"R0":     0x00,
+		"R1":     0x01,
+		"R2":     0x02,
+		"R3":     0x03,
+		"R4":     0x04,
+		"R5":     0x05,
+		"R6":     0x06,
+		"R7":     0x07,
+		"R8":     0x08,
+		"R9":     0x09,
+		"R10":    0x0a,
+		"R11":    0x0b,
+		"R12":    0x0c,
+		"R13":    0x0d,
+		"R14":    0x0e,
+		"R15":    0x0f,
 		"SCREEN": 0x4000,
-		"KBD": 0x6000,
+		"KBD":    0x6000,
 	}
 
-	file, err := os.Open("assembly/pong/Pong.asm")
-	//file, err := os.Open("assembly/add/Add.asm")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	var files = os.Args[1:]
+	for _, arg := range files {
+		var start = time.Now()
 
-	scanner := bufio.NewScanner(file)
+		file, err := os.Open(arg)
 
-	for scanner.Scan() {
-		var line = scanner.Text()
-
-		var commentIndex = strings.Index(line, "//")
-		if commentIndex != -1 {
-			line = line[:commentIndex]
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		line = strings.Trim(line, " ")
+		scanner := bufio.NewScanner(file)
 
-		if len(line) == 0 {
-			continue
-		}
+		for scanner.Scan() {
+			var line = scanner.Text()
 
-		lines = append(lines, line)
-	}
-
-	var instructionNumber = 0
-	for _, line := range lines {
-		if line[0] == '(' {
-			var end = strings.IndexRune(line, ')')
-			var symbol = line[1:end]
-			labels[symbol] = instructionNumber
-			continue
-		}
-		instructionNumber++
-	}
-
-	for _, line := range lines {
-		if line[0] == '(' {
-			continue
-		}
-
-		if line[0] == '@' {
-			var value = line[1:]
-
-			if num, err := strconv.Atoi(value); err == nil {
-				str := fmt.Sprintf("%016b", num)
-				output = append(output, str)
-			} else {
-				var addressOrValue int
-
-				if val, found := predefinedSymbols[value]; found {
-					addressOrValue = val
-				} else if variables[value] >= 16 {
-					addressOrValue = variables[value]
-				} else if labels[value] > 0 {
-					addressOrValue = labels[value]
-				} else {
-					addressOrValue = variablesMemoryStart + len(variables);
-					variables[value] = addressOrValue
-				}
-
-				str := fmt.Sprintf("%016b", addressOrValue)
-				output = append(output, str)
+			var commentIndex = strings.Index(line, "//")
+			if commentIndex != -1 {
+				line = line[:commentIndex]
 			}
 
-			continue
+			line = strings.Trim(line, " ")
+
+			if len(line) == 0 {
+				continue
+			}
+
+			lines = append(lines, line)
 		}
 
-		var res = parseCInstruction(line)
-		var str = fmt.Sprintf("%016b", res)
-		output = append(output, str)
-	}
+		var instructionNumber = 0
+		for _, line := range lines {
+			if line[0] == '(' {
+				var end = strings.IndexRune(line, ')')
+				var symbol = line[1:end]
+				labels[symbol] = instructionNumber
+				continue
+			}
+			instructionNumber++
+		}
 
-	f, err := os.Create("bin/Pong.hack")
-	defer f.Close()
+		for _, line := range lines {
+			if line[0] == '(' {
+				continue
+			}
 
-	for _, str := range output{
-		f.WriteString(str + "\n")
-	}
+			if line[0] == '@' {
+				var value = line[1:]
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+				if num, err := strconv.Atoi(value); err == nil {
+					str := fmt.Sprintf("%016b", num)
+					output = append(output, str)
+				} else {
+					var addressOrValue int
+
+					if val, found := predefinedSymbols[value]; found {
+						addressOrValue = val
+					} else if variables[value] >= 16 {
+						addressOrValue = variables[value]
+					} else if labels[value] > 0 {
+						addressOrValue = labels[value]
+					} else {
+						addressOrValue = variablesMemoryStart + len(variables)
+						variables[value] = addressOrValue
+					}
+
+					str := fmt.Sprintf("%016b", addressOrValue)
+					output = append(output, str)
+				}
+
+				continue
+			}
+
+			var res = parseCInstruction(line)
+			var str = fmt.Sprintf("%016b", res)
+			output = append(output, str)
+		}
+
+		var filename = filepath.Base(arg)
+		filename = strings.TrimSuffix(filename, filepath.Ext(filename))
+
+		elapsed(fmt.Sprintf("Translation from %s to bin/%s.hack", arg, filename), start)
+
+		f, err := os.Create(fmt.Sprintf("bin/%s.hack", filename))
+
+		for _, str := range output {
+			_, _ = f.WriteString(fmt.Sprintf("%s\n", str))
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		_ = f.Close()
+		_ = file.Close()
 	}
 }
 
@@ -147,90 +166,90 @@ func parseCInstruction(line string) int {
 	switch computation {
 	case "0":
 		out |= 0b0000_1010_1000_0000
-		break;
+		break
 	case "1":
 		out |= 0b0000_1111_1100_0000
-		break;
+		break
 	case "-1":
 		out |= 0b0000_1110_1000_0000
-		break;
+		break
 	case "D":
 		out |= 0b0000_0011_0000_0000
-		break;
+		break
 	case "A":
 		out |= 0b0000_1100_0000_0000
-		break;
+		break
 	case "!D":
 		out |= 0b0000_0011_0100_0000
-		break;
+		break
 	case "!A":
 		out |= 0b0000_1100_0100_0000
-		break;
+		break
 	case "-D":
 		out |= 0b0000_0011_1100_0000
-		break;
+		break
 	case "-A":
 		out |= 0b0000_1100_1100_0000
-		break;
+		break
 	case "D+1":
 		out |= 0b0000_0111_1100_0000
-		break;
+		break
 	case "A+1":
 		out |= 0b0000_1101_1100_0000
-		break;
+		break
 	case "D-1":
 		out |= 0b0000_0011_1000_0000
-		break;
+		break
 	case "A-1":
 		out |= 0b0000_1100_1000_0000
-		break;
+		break
 	case "D+A":
 		out |= 0b0000_0000_1000_0000
-		break;
+		break
 	case "D-A":
 		out |= 0b0000_0100_1100_0000
-		break;
+		break
 	case "A-D":
 		out |= 0b0000_0001_1100_0000
-		break;
+		break
 	case "D&A":
 		out |= 0b0000_0000_0000_0000
-		break;
+		break
 	case "D|A":
 		out |= 0b0000_0101_0100_0000
-		break;
+		break
 
 	// "a" flag set to 1
 	case "M":
 		out |= 0b0001_1100_0000_0000
-		break;
+		break
 	case "!M":
 		out |= 0b0001_1100_0100_0000
-		break;
+		break
 	case "-M":
 		out |= 0b0001_1100_1100_0000
-		break;
+		break
 	case "M+1":
 		out |= 0b0001_1101_1100_0000
-		break;
+		break
 	case "M-1":
 		out |= 0b0001_1100_1000_0000
-		break;
+		break
 	case "D+M":
 		out |= 0b0001_0000_1000_0000
-		break;
+		break
 	case "D-M":
 		out |= 0b0001_0100_1100_0000
-		break;
+		break
 	case "M-D":
 		out |= 0b0001_0001_1100_0000
-		break;
+		break
 	case "D&M":
 		out |= 0b0001_0000_0000_0000
-		break;
+		break
 	case "D|M":
 		out |= 0b0001_0101_0100_0000
-		break;
+		break
 	}
 
 	if isStore {
@@ -288,4 +307,8 @@ func parseCInstruction(line string) int {
 	}
 
 	return out
+}
+
+func elapsed(what string, start time.Time) {
+	fmt.Printf("%s took %v\n", what, time.Since(start))
 }
